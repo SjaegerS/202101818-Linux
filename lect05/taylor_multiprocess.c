753fe750ff8c33e8b742d9fee772f632c469a3d0
Jinyoung Choi <wlsdud0225@smail.kongju.ac.kr>
2025-10-24 00:30:37 +0900

    newfileadd-taylor_multiprocess.c

--- lect05/taylor_multiprocess.c
+++ lect05/taylor_multiprocess.c
@@ -1,56 +1,63 @@
 #include <stdio.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define N 4
+#define MAXLINE 100
 
 void sinx_taylor(int num_elements, int terms, double* x, double* result)
 {
-	int pipes[N][2];
-	pid_t pid;
+	int fd[2*N];
+	int pid;
+	char message[MAXLINE], line[MAXLINE];
+	int length;
+	int child_id;
 
 	for(int i = 0; i<num_elements; i++){
-		pipe(pipes[i]);
+		pipe(fd + 2*i);
+
+		child_id = i;
 		pid = fork();
-
-		if(pid ==0)
-		{
-			close(pipes[i][0]);
-
-			double value = x[i];
-			double numer = x[i] * x[i] * x[i];
-			double denom = 6.;
-			int sign = -1;
-
-			for(int j=1; j<=terms; j++){
-				value += (double)sign * numer / denom;
-				numer *= x[i] * x[i];
-				denom *= (2.*(double)j+2.) * (2.*(double)j +3);
-				sign *= -1;
-			}
-
-			write(pipes[i][1], &value, sizeof(double));
-			close(pipes[i][1]);
-			exit(0);
+		if(pid ==0) {
+			break;
 		} else {
-			close(pipes[i][1]);
+			close(fd[2*i+1]);
 		}
-		
 	}
 
-	for(int i = 0; i < num_elements; i++)
-	{
-		read(pipes[i][0], &result[i], sizeof(double));
-		close(pipes[i][0]);
-	}
+	if(pid ==0){
+		close(fd[2*child_id]);
+		double value = x[child_id];
+		double numer = x[child_id] * x[child_id] * x[child_id];
+		double denom = 6.;
+		int sign = -1;
 
-	for (int i =0; i < num_elements; i++)
-	{
-		wait(NULL);
-	}
+		for(int j=1; j<=terms; j++){
+			value += (double)sign * numer / denom;
+			numer *= x[child_id] * x[child_id];
+			denom *= (2.*(double)j+2.) * (2.*(double)j +3);
+			sign *= -1;
+		}
+		result[child_id] = value;
+		sprintf(message, "%lf", result[child_id]);
+		length = strlen(message)+1;
+		write(fd[2*child_id+1], message, length);
+		exit(child_id);
+	}else {
+		for(int i=0;i<num_elements; i++){
+			int status;
+			wait(&status);
+			int child_id= status >> 8;
+			read(fd[2*child_id], line, MAXLINE);
+			result[child_id] = atof(line);
+		}
+
+	}	
+	
 }
 
 int main()
